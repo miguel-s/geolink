218e29549552adb17b16eda0d2298fd00bd7f335
Miguel Serrano <mlsv89@gmail.com>
2016-11-14 15:46:05 +0100

    improved user db seeding

--- scripts/seedUsers.js
+++ scripts/seedUsers.js
@@ -1,8 +1,9 @@
 'use strict';
 
+const path = require('path');
 const sqlite = require('sqlite3').verbose();
 
-const db = new sqlite.Database('../database.db');
+const db = new sqlite.Database(path.resolve(__dirname, '../database.db'));
 const users = [
   {
     $id: '009ef00e-52ca-4c62-8cc6-fb48a4b4463b',
@@ -10,6 +11,13 @@ const users = [
     $password: '$2a$12$9ic5IkbDq9MnmD1JZs7yP.wQPqiosJkS/q1nmQzgN5IuQ3dydljli',
     $email: 'foo@hapiu.com',
     $scope: 'user, admin',
+  },
+  {
+    $id: 'e6ce12a8-7bb1-4e88-8c3a-846b0db1c2a7',
+    $username: 'Bar',
+    $password: '$2a$12$nflxrLTjE30KbdgmhHcO2ugQDzeE3W4cFsI1FjNqcy2Zj0sGO7qlG',
+    $email: 'bar@hapiu.com',
+    $scope: 'user',
   },
 ];
 
