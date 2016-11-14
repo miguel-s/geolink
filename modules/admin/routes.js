863d4522d05ef7fc664751bdc0e215ff92b47aae
Miguel Serrano <mlsv89@gmail.com>
2016-11-14 15:46:25 +0100

    basic admin module

--- modules/admin/routes.js
+++ modules/admin/routes.js
@@ -6,6 +6,7 @@ module.exports = [
     path: '/',
     config: {
       description: 'Returns the index page',
+      auth: { strategy: 'session', mode: 'try', scope: 'admin' },
       handler: {
         view: {
           template: 'index',
