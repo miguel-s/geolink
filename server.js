afb8ddbf2338eb83a7b8148fd1649817c969ba89
Miguel Serrano <mlsv89@gmail.com>
2016-11-15 10:46:47 +0100

    added index route

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ exports.init = (manifest, options, next) => {
   Glue.compose(manifest, options, (err, server) => {
     Hoek.assert(!err, err);
 
+    server.route([
+      {
+        method: 'GET',
+        path: '/',
+        config: {
+          description: 'Returns the index page',
+          auth: { strategy: 'session', mode: 'try' },
+          handler: function(request, reply) {
+            reply.redirect('/dashboards');
+          },
+        },
+      },
+    ]);
+
     server.start(err => next(err, server));
   });
 };
